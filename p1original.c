f4c13ce79d44d8aa4800d6285d7326650410e750
aksharareddyt <93091145+aksharareddyt@users.noreply.github.com>
2022-02-02 15:34:43 +0530

    Update p1original.c

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-
+#include <stdio.h>
+void input(int *pa,int *pb)
+{
+  printf("Enter the number\n");
+  scanf("%d%d",pa,pb);
+}
+void add(int a, int b, int *sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("%d+%d is %d",a,b,sum);
+}
+int main()
+{
+  int x,y,z;
+  input(&x,&y);
+  add(x,y,&z);
+  output(x,y,z);
+  return 0;
 }
